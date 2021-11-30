a678774d8b9b83c11f5788af847d886b9ef49ad1
imyp92 <pyw5420@gmail.com>
2021-11-30 21:58:17 +0900

    define TweetUpdateDto

---
+++ src/main/java/com/yp/twitterclonebackend/dto/TweetUpdateDto.java
@@ -0,0 +1,16 @@
+package com.yp.twitterclonebackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TweetUpdateDto {
+
+    private String text;
+
+}
